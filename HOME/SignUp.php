c879c6a69a25c1781dd53e1da638c26f2d45b232
Kshitiz-kothari31 <kshitizkothari69@gmail.com>
2024-06-09 23:57:46 +0530

    changes files extension

--- HOME/SignUp.php
+++ HOME/SignUp.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="styles/signup.css">
 </head>
 <body>
-    <form action="../Backend/SignUp.php" method="post">
+    <form action="SignUp.php" method="post">
         <div class="container">
             <img src="images/login_page_image.png" alt="this is an image.">
             <div class="SignupBox">
@@ -29,5 +29,44 @@
             </div>
         </div>
     </form>
+
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "socialnetwork";
+    
+        //creating the connection.
+        $conn = mysqli_connect($servername, $username, $password, $database);
+    
+        //Checking connection.
+        if( !$conn ){
+            die("Sorry we failed to connect : ".mysqli_connect_error());
+        }
+
+        if( $_SERVER["REQUEST_METHOD"] == "POST"){
+            $Fname = $_POST['Fname'];
+            $Lname = $_POST['Lname'];
+            $Email = $_POST['Email'];
+            $Pnumber = $_POST['Pnumber'];
+            $RememberMe = $_POST['Check'];
+            $Password;  
+        
+            if( $_POST['Password'] == $_POST['Cpassword'] ){
+                $Password = $_POST['Password'];
+            }
+
+            $sql = "INSERT INTO `Users data`( `FirstName`, `LastName`, `Email`, `PhoneNumber`, `Password`) 
+                VALUES ('$Fname', '$Lname', '$Email', '$Pnumber', '$Password')";
+            
+            if(mysqli_query($conn, $sql))
+            {
+                mysqli_close($conn);
+            }else{
+                mysqli_close($conn);
+            }
+        }
+
+    ?>
 </body>
 </html>
