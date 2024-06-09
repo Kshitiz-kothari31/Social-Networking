2475ec0d2b97ebf9577b84eb8263f88901e4a608
Kshitiz-kothari31 <kshitizkothari69@gmail.com>
2024-06-09 20:45:43 +0530

    changes the fileextension and add the backend file

---
+++ Backend/createDatabase.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    //creating the connection.
+    $conn = mysqli_connect($servername, $username, $password);
+
+    //Checking connection.
+    if( !$conn ){
+        die("Sorry we failed to connect : ".mysqli_connect_error());
+    }
+
+    $sql = "CREATE DATABASE socialNetwork";
+    if( !(mysqli_query($conn, $sql))){
+        echo "error".mysqli_error();
+    }
+?>
---
+++ Backend/createTable.php
@@ -0,0 +1,19 @@
+<?php
+        
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "socialNetwork";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if( !$conn ){
+        die("Sorry we failed to connect : ".mysqli_connect_error());
+    }
+
+    $sql = "CREATE TABLE `userData`( `UserName` varchar(12) NOT NULL,  `Password` varchar(10), PRIMARY KEY (`UserName`))";
+
+    if( !(mysqli_query($conn, $sql))){
+        echo "error".mysqli_error();
+    }
+?>
